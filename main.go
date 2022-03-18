50b3a3832a35e023579c058467725140d0171ccb
Evie <evangeliapal@gmail.com>
2022-03-18 15:49:21 +0000

    Refactor returnSpotsWithDB method to parse the query parameters

--- main.go
+++ main.go
@@ -4,9 +4,9 @@ import (
 	"database/sql"
 	"encoding/json"
 	"fmt"
-	"io/ioutil"
 	"log"
 	"net/http"
+	"strconv"
 
 	_ "github.com/lib/pq"
 )
@@ -36,68 +36,45 @@ func main() {
 	}
 
 	fmt.Println("Successfully connected!")
-
-	// spots, err := findSpots(db, 23.7249394, 37.9782996, 3000, "circle")
-	// if err != nil {
-	// 	panic(err)
-	// }
-	// fmt.Println(spots)
-
 	handleRequests(db)
-	
-	res, err := http.Get("https://fakerapi.it/api/v1/custom?_quantity=1&latitude=latitude&longitude=longitude&radius=number")
-	if err !=nil {
-		log.Fatal(err)
-	}
-	defer res.Body.Close()
-
-	body, err := ioutil.ReadAll(res.Body)
-	if err !=nil {
-		log.Fatal(err)
-	}
-
-	var response Response
-	json.Unmarshal(body, &response)
-
-	for p := range response.Coordinates {
-		fmt.Println("Latitude:", p.Latitude)
-		fmt.Println("Longitude:", p.Longitude)
-		fmt.Println("Radius:", p.Radius)
-	}
 }
 
-func homePage(w http.ResponseWriter, r *http.Request){
-    fmt.Fprintf(w, "Welcome to the HomePage!")
-    fmt.Println("Endpoint Hit: homePage")
+func homePage(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "Welcome to the HomePage!")
+	fmt.Println("Endpoint Hit: homePage")
 }
 
 func returnSpotsWithDB(db *sql.DB) func(w http.ResponseWriter, r *http.Request) {
-	return func(w http.ResponseWriter, r *http.Request){
-      fmt.Println("Endpoint Hit: returnSpots")
-      spots, err := findSpots(db, 23.7249394, 37.9782996, 3000, "circle")
-      if err != nil {
-          panic(err)
-      }
-      fmt.Println(spots)
-      json.NewEncoder(w).Encode(spots)
-  }
+	return func(w http.ResponseWriter, r *http.Request) {
+		query := r.URL.Query()
+		lat, _ := strconv.ParseFloat(query.Get("latitude"), 64)
+		long, _ := strconv.ParseFloat(query.Get("logitude"), 64)
+		radius, _ := strconv.ParseFloat(query.Get("radius"), 64)
+		radType := query.Get("type")
+		spots, err := findSpots(db, float32(lat), float32(long), float32(radius), string(radType))
+		if err != nil {
+			panic(err)
+		}
+		fmt.Println(spots)
+		json.NewEncoder(w).Encode(spots)
+	}
 }
 
 func handleRequests(db *sql.DB) {
-    http.HandleFunc("/", homePage)
-    http.HandleFunc("/spots", returnSpotsWithDB(db))
-    log.Fatal(http.ListenAndServe(":10000", nil))
+	http.HandleFunc("/", homePage)
+	http.HandleFunc("/spots", returnSpotsWithDB(db))
+	log.Fatal(http.ListenAndServe(":10000", nil))
 }
 
 type Response struct {
-	Status string
-	Code string
-	Total int
+	Status      string
+	Code        string
+	Total       int
 	Coordinates []Coordinate `json:"data"`
 }
 
 type Coordinate struct {
-	Latitude float64
+	Latitude  float64
 	Longitude float64
-	Radius int
+	Radius    int
 }
